be6ff463a32a104b4ced9b34f14f77f67d241147
James Damore <JamesDamore@gmail.com>
2018-08-28 18:19:17 -0500

    Add scripts to update and restart server

---
+++ build/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Gets the script's directory, for explanation of command, see:
+# https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+GIT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+
+git -C $GIT_DIR/etherpad-lite fetch
+git -C $GIT_DIR/etherpad-lite checkout $1
+git -C $GIT_DIR/etherpad-lite pull
+
+wait
+
+$SCRIPT_DIR/restart.sh
