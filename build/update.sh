2e58b799bd8cbc5700774de411e23756e35fba6b
root <root@ip-172-31-3-69.us-west-1.compute.internal>
2019-02-05 23:23:40 +0000

    Fix git directory in update script

--- build/update.sh
+++ build/update.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Gets the script's directory, for explanation of command, see:
 # https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-GIT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+GIT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 
 git -C $GIT_DIR/etherpad-lite fetch
 git -C $GIT_DIR/etherpad-lite checkout $1
