593fc39d4c57b665c25b512ebd37f3e26f3f2f8f
root <root@ip-172-31-3-69.us-west-1.compute.internal>
2019-02-05 23:20:21 +0000

    Remove git fetch in restart script

--- build/restart.sh
+++ build/restart.sh
@@ -1,15 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Gets the script's directory, for explanation of command, see:
 # https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BIN_DIR="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" && pwd)"/bin
 
-GIT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
-
-git -C $GIT_DIR/etherpad-lite fetch
-git -C $GIT_DIR/etherpad-lite checkout $1
-git -C $GIT_DIR/etherpad-lite pull
-
-wait
-
-$SCRIPT_DIR/restart.sh
+$BIN_DIR/screenRun.sh
