9f2efec2624a8feb8239b062e1d8adafc4636d52
pauln-esper <paul@esper.com>
2019-09-20 12:00:04 -0700

    switch to use log4js logging rather than console

--- plugins_available/ep_healthcheck/healthcheck.js
+++ plugins_available/ep_healthcheck/healthcheck.js
@@ -1,5 +1,7 @@
 
 var db = require('ep_etherpad-lite/node/db/DB');
+var log4js = require('log4js');
+var logger = log4js.getLogger("healthcheck");
 
 var lastCheckTime = null;
 var lastStatusCode = 200;
@@ -11,15 +13,15 @@ exports.expressCreateServer = function (hook_name, args, cb) {
         // dont hit this all the time
         var now = Date.now();
         if (lastCheckTime == null || now - lastCheckTime > 30000) {
-            console.log("running check  " + (now - lastCheckTime));
+            logger.debug("running check  " + (now - lastCheckTime));
             db.db.get("pad:0", function (err, pad) {
-                console.log("err " + err);
-                console.log("pad " + pad);
+                logger.debug("err " + err);
+                logger.debug("pad " + pad);
 
                 // expect pad will be undefined, but don't really care
                 // expect err to be null unless db is broken
                 if (err != null) {
-                    console.log("got a db error" + err);
+                    logger.error("got a db error" + err);
                     lastStatusCode = 500;
                     lastMessage = "ERROR\ndatabase connectivity issue";
                 } else {
